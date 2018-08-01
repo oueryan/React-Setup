0fb2f509274b3e45431967d9de106eb38a111c87
oueryan <Ryan@Ryans-MacBook-Pro.local>
2018-08-01 17:06:48 +0800

    stripe course

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import './Header.css'
+import StripeScheckout from 'react-stripe-checkout'
 
 class Header extends React.Component {
   constructor(props) {
@@ -25,6 +26,24 @@ class Header extends React.Component {
     ]
   }
 
+  handlePurchase = (token) => {
+    const amount = 5000
+    const description = "My awesome product"
+
+    const bodyObject = {
+      tokenId: token.id,
+      email: token.email,
+      name: token.name,
+      description,
+      amount
+    }
+
+    fetch('http://localhost:9000/stripe-charge', {
+      method: 'POST',
+      body: JSON.stringify(bodyObject)
+    })
+  }
+
   render() {
     return (
       <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
@@ -33,7 +52,13 @@ class Header extends React.Component {
           <Link to="/courses">Courses</Link>
           <Link to="/downloads">Downloads</Link>
           <Link to="/workshops">Workshops</Link>
-          <Link to="/buy"><button>Buy</button></Link>
+          <StripeScheckout
+            amount={5000}
+            image="https://cl.ly/0K2f1V3K3h0D/download/Logo.jpg"
+            token={this.handlePurchase}
+            stripeKey={'pk_test_DuyxfsT7HKrqqI13RNpRntCF'}>
+            <button>Buy</button>
+          </StripeScheckout>
         </div>
       </div>
     )
